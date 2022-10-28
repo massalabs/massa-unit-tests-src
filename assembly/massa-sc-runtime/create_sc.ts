5f549e2c07b9fc61294c4b745be74ad10320142b
Sydhds <sylvain.delhomme@gmail.com>
2022-10-28 16:42:37 +0200

    Fix source using fileToBase64() (round 3) (#6)
    Co-authored-by: sydhds <sydhds@massa.net>

--- assembly/massa-sc-runtime/create_sc.ts
+++ assembly/massa-sc-runtime/create_sc.ts
@@ -2,7 +2,7 @@ import { createSC, fileToBase64 } from "@massalabs/massa-as-sdk";
 
 export function main(_args: string): i32 {
     // Create smart contract "get_string"
-    const bytes = fileToBase64('./build/get_string.wasm');
+    const bytes = fileToBase64('./build/massa-sc-runtime/get_string.wasm');
     createSC(bytes);
     return 0;
 }
