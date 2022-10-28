57dd52e61fb49881d0b9fe38169d9a8733adca43
Sydhds <sylvain.delhomme@gmail.com>
2022-10-28 17:45:08 +0200

    Fix source using fileToBase64() (round 4) (#7)
    Co-authored-by: sydhds <sydhds@massa.net>

--- assembly/massa/send_message/send_message.ts
+++ assembly/massa/send_message/send_message.ts
@@ -6,7 +6,8 @@
 import { sendMessage, print, createSC, fileToBase64 } from "@massalabs/massa-as-sdk"
 
 export function main(name: string): void {
-    const bytes = fileToBase64('./build/receive_message.wasm');
+
+    const bytes = fileToBase64('./build/massa/receive_message.wasm');
     const address = createSC(bytes);
     sendMessage(address, "receive", 1, 1, 20, 1, 100_000, 1, 100, "hello my good friend!");
     print("receiver created and message sent")
