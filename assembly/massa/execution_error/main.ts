10fe3d4f3f30f82167a1048b9b3a45cadbe936e0
Thomas Plisson <thomas.plisson@epitech.eu>
2022-12-08 17:40:55 +0100

    Add an event before the failure

--- assembly/massa/execution_error/main.ts
+++ assembly/massa/execution_error/main.ts
@@ -3,7 +3,8 @@ import { generateEvent, call, Address, Args } from "@massalabs/massa-as-sdk";
 export function main(_args: string): void {
     let addr = new Address("invalid_addr");
     let args = new Args();
+    generateEvent("event generated before the sc failure");
     args.add("invalid_param");
     call(addr, "invalid_func", args, 42);
-    generateEvent("Hello world!");
+    generateEvent("event generated after the sc failure");
 }
