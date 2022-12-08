98ac7a689f9382f8c54972a908306218a5dfa410
sydhds <sydhds@massa.net>
2022-12-08 10:47:48 +0100

    Update datastore manipulations with clearer static array init

--- assembly/massa/datastore_manipulations/main.ts
+++ assembly/massa/datastore_manipulations/main.ts
@@ -11,22 +11,24 @@ import {Storage, getKeys, getKeysOf, generateEvent, Context, print} from "@massa
 
 export function main(_args: string): void {
 
-    const key1 = new StaticArray<u8>(4).fill(1, 0, 4);
-    const key2 = new StaticArray<u8>(4).fill(2, 0, 4);
-    const value1 = new StaticArray<u8>(4).fill(21, 0, 4);
-    const value2 = new StaticArray<u8>(4).fill(42, 0, 4);
+    const key1: StaticArray<u8> = [1, 0, 4, 255];
+    const key2: StaticArray<u8> = [2, 0, 254, 255];
+    const value1: StaticArray<u8> = [21, 0, 49];
+    const value2: StaticArray<u8> = [42, 0, 48];
     Storage.set(key1, value1);
     Storage.set(key2, value2);
     Storage.del(key2);
 
     let keys = getKeys();
     let msg = `keys: ${keys}`;
+    // print(msg);
     generateEvent(msg);
 
     let addr_ = Context.callee();
     let addr = addr_.toByteString();
     let keys2 = getKeysOf(addr);
     let msg2 = `keys2: ${keys2}`;
+    // print(msg2);
     generateEvent(msg2);
 }
 
