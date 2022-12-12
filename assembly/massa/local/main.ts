abd150ddac269e0f62134130d968830339df97c4
Thomas Plisson <thomas.plisson@epitech.eu>
2022-12-12 14:52:52 +0100

    test code for local execution testing

---
+++ assembly/massa/local/main.ts
@@ -0,0 +1,10 @@
+import { generateEvent, Address, Args, local_call, local_execution } from "@massalabs/massa-as-sdk";
+
+export function main(_args: string): void {
+    let addr = new Address("addr");
+    let args = new Args();
+    generateEvent("event generated before the sc");
+    args.add("param");
+    local_call(addr, "unc", args);
+    generateEvent("event generated after the sc");
+}
