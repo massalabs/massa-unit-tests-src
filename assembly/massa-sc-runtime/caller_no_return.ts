98f6126406f44321f253562170e035f369066f6b
sydhds <sd@massa.net>
2023-01-25 11:26:27 +0100

    Remove outdated SC for massa-sc-runtime

--- assembly/massa-sc-runtime/caller_no_return.ts
+++
@@ -1,9 +0,0 @@
-import { print, call, Address, Args } from "@massalabs/massa-as-sdk";
-
-export function main(): void {
-  let addr = new Address("get_string");
-  let args = new Args();
-  args.add("you");
-  let string_from = call(addr, "helloName", args, 0);
-  print(string_from.toString());
-}
--- assembly/massa-sc-runtime/receive_message.ts
+++
@@ -1,9 +0,0 @@
-/** ***********************
- * Smart contract containing a message handler function
- **/
-
-import { print } from "@massalabs/massa-as-sdk"
-
-export function receive(data: string): void {
-    print("message received: " + data);
-}
