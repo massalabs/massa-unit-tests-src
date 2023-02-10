9521cabd66a4d9c41b3d31c1c13d701f00a3f8b4
AurelienFT <aurelien.foucault@epitech.eu>
2023-02-10 11:39:45 +0100

    New version nested_calls

--- assembly/massa/nested_call/test.ts
+++ assembly/massa/nested_call/test.ts
@@ -5,20 +5,19 @@
 import { print, generateEvent, call, Context, Address } from "@massalabs/massa-as-sdk"
 import { Args } from '@massalabs/as-types';
 
-export function receive(data: string): void {
+export function receive(data: StaticArray<u8>): void {
     print("Gas at start inner: " + Context.remainingGas().toString());
     generateEvent(Context.remainingGas().toString());
-    print(data);
     print("Gas at end inner: " + Context.remainingGas().toString());
     generateEvent(Context.remainingGas().toString());
 }
 
-export function test(address: string): void {
+export function test(_: StaticArray<u8>): void {
     print("Gas at start: " + Context.remainingGas().toString());
     generateEvent(Context.remainingGas().toString());
     let args = new Args();
     args.add("massa");
-    call(Address.fromByteString(address), "receive", args, 0);
+    call(Context.callee(), "receive", args, 0);
     print("Gas at end: " + Context.remainingGas().toString());
     generateEvent(Context.remainingGas().toString());
 }
