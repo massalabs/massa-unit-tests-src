838cd75b6ce6e2917c6b08e6ba6ddd3ee0d7466c
modship <yeskinokay@gmail.com>
2024-08-28 16:54:10 +0200

    update param order

--- assembly/massa/deferred_call/register.ts
+++ assembly/massa/deferred_call/register.ts
@@ -4,7 +4,7 @@ import { generateEvent, Context, deferredCallRegister } from "@massalabs/massa-a
 
 
 export function main(name: string): void {
-    let r = deferredCallRegister("AS12jc7fTsSKwQ9hSk97C3iMNgNT1XrrD6MjSJRJZ4NE53YgQ4kFV", "receive", 1, 1, 300_000, 10000000000, []);
+    let r = deferredCallRegister("AS12jc7fTsSKwQ9hSk97C3iMNgNT1XrrD6MjSJRJZ4NE53YgQ4kFV", "receive", 1, 1, 300_000, [], 10000000000);
     generateEvent("Deferred call registered");
     generateEvent(r.toString());
 }
