f6e9c6c2e29d254a1cce56b8cad3e1e7a1ace7d0
modship <yeskinokay@gmail.com>
2024-10-03 09:55:11 +0200

    update massa sdk

--- assembly/massa/deferred_call/register.ts
+++ assembly/massa/deferred_call/register.ts
@@ -1,10 +1,11 @@
 import { Args } from "@massalabs/as-types";
-import { generateEvent, Context, deferredCallRegister } from "@massalabs/massa-as-sdk";
+import { generateEvent, Context, deferredCallRegister, Slot } from "@massalabs/massa-as-sdk";
 
 
 
 export function main(name: string): void {
-    let r = deferredCallRegister("AS12jc7fTsSKwQ9hSk97C3iMNgNT1XrrD6MjSJRJZ4NE53YgQ4kFV", "receive", 1, 1, 300_000, [], 10000000000);
+    let slot = new Slot(1, 1);
+    let r = deferredCallRegister("AS12jc7fTsSKwQ9hSk97C3iMNgNT1XrrD6MjSJRJZ4NE53YgQ4kFV", "receive", slot, 300_000, [], 10000000000);
     generateEvent("Deferred call registered");
     generateEvent(r);
 }
--- assembly/massa/deferred_call/register_fail.ts
+++ assembly/massa/deferred_call/register_fail.ts
@@ -1,5 +1,5 @@
 import { Args } from "@massalabs/as-types";
-import { generateEvent, Context, deferredCallRegister } from "@massalabs/massa-as-sdk";
+import { generateEvent, Context, deferredCallRegister, Slot } from "@massalabs/massa-as-sdk";
 
 
 
@@ -7,6 +7,6 @@ export function main(name: string): void {
     generateEvent("try Deferred call register");
     // we want to book all max async gas, but in the slot 1,10 we already have a deferred call that uses 500 gas
     // so this call will fail
-    let r = deferredCallRegister("AS12jc7fTsSKwQ9hSk97C3iMNgNT1XrrD6MjSJRJZ4NE53YgQ4kFV", "receive", 1, 10, 1_000_000_000, [], 100);
+    let r = deferredCallRegister("AS12jc7fTsSKwQ9hSk97C3iMNgNT1XrrD6MjSJRJZ4NE53YgQ4kFV", "receive", new Slot(1, 10), 1_000_000_000, [], 100);
     generateEvent(r.toString());
 }
